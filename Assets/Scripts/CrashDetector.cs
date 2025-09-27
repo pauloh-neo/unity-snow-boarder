8efba002f33429eca40c80e7b7eecf74fa9c86af
unknown <paulo.henriqq8@gmail.com>
2025-09-27 12:08:00 -0400

    Take a moment to Tune the game

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,12 +4,14 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float reloadDelay = 1f;
+    [SerializeField] ParticleSystem crashParticleEffector;
     void OnTriggerEnter2D(Collider2D collision)
     {
         float layerIndex = LayerMask.NameToLayer("Floor");
 
         if (collision.gameObject.layer == layerIndex)
         {
+            crashParticleEffector.Play();
             Invoke("ReloadScene", reloadDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,12 +4,14 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float reloadDelay = 1f;
+    [SerializeField] ParticleSystem finishParticleEffector;
     void OnTriggerEnter2D(Collider2D collision)
     {
         float layerIndex = LayerMask.NameToLayer("Player");
 
         if (collision.gameObject.layer == layerIndex)
         {
+            finishParticleEffector.Play();
             Invoke("ReloadScene", reloadDelay);
         }
     }
