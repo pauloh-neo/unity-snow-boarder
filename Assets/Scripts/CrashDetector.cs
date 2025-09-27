a0178c0e913da73bb453c8de7a9bb3d439e6b833
unknown <paulo.henriqq8@gmail.com>
2025-09-27 11:03:02 -0400

    Add Invoke to reloadDelay

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         float layerIndex = LayerMask.NameToLayer("Floor");
 
         if (collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         float layerIndex = LayerMask.NameToLayer("Player");
 
         if (collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        
+        SceneManager.LoadScene(0);
     }
 }
